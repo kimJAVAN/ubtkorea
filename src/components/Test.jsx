511c54cd50f0a3fb3eec6093de85f5e8fd63fce4
shin-hyun-min <gusals0702@naver.com>
2024-05-18 16:15:09 +0900

    PC/모바일 감지 vh 문제 대응 용 코드 추가

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-export default function Test() {
+export default function Test(props) {
+  const {
+    windowWidth
+  } = props
+
   return (
     <div>testtesttesttest</div>
   );
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "../App.jsx";
 import MainRouter from "./MainRouter.jsx";
@@ -8,12 +8,28 @@ import Test from "../components/Test.jsx";
 
 
 function AppRouter() {
+  // 디바이스 width 감지 - PC 모바일 감지용
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const handleResize = () => setWindowWidth(window.innerWidth);
+
+  // 화면 반응형 대응 용
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+
+    // 모바일 vh 문제 용
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
+
   return (
     <Router>
       <Routes>
         <Route path="/">
           <Route index  element={<App />}></Route>
-          <Route path={'test/*'} element={<MainRouter />}>
+          <Route path={'test/*'} element={<MainRouter windowWidth={windowWidth} />}>
           </Route>
         </Route>
       </Routes>
